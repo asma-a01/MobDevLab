4a6ed7b49ba75a32939a73229f6da3a2ca699e0c
asma-a01 <asma.asim204@gmail.com>
2025-05-05 01:26:46 +0500

    Add Flutter project files

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# MobDevLab
+# flutter_application_1
+
+Flutter Hero Animation App:
+
+A simple Flutter app showing hero animation with local images (Sunset, Mountain, River). Tap an image in the list to see it bigger with a smooth transition.
+
+Features:
+
+1. List of images with titles.
+2. Hero animation for image transition.
+3. Uses local images from assets/images/.
+
+Setup:
+
+1. Clone the repo:
+
+git clone https://github.com/asma-a01/MobDevLab.git
+
+2. Go to the folder:
+
+cd MobDevLab
+
+3. Install dependencies:
+
+flutter pub get
+
+How to Run:
+1. Add images (img1.jpg, img2.jpg, img3.jpg) to assets/images/.
+
+2. Update pubspec.yaml:
+
+flutter:
+  assets:
+    - assets/images/
+
+3. Run the app:
+
+flutter run
+
+Files:
+
+1. lib/lab07.dart: Main app code.
+
+2. assets/images/: Folder for images.
---
+++ lib/assets/images/lab07.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hero Animation with Assets',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: ItemListScreen(),
+    );
+  }
+}
+
+class ItemListScreen extends StatelessWidget {
+  final List<Map<String, String>> items = [
+    {'id': '1', 'title': 'Sunset', 'image': 'assets/images/img1.jpeg'},
+    {'id': '2', 'title': 'Mountain', 'image': 'assets/images/img2.jpeg'},
+    {'id': '3', 'title': 'River', 'image': 'assets/images/img3.jpeg'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My Local Images')),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return ListTile(
+            leading: Hero(
+              tag: item['id']!,
+              child: Image.asset(
+                item['image']!,
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+              ),
+            ),
+            title: Text(item['title']!),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => DetailScreen(item: item),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final Map<String, String> item;
+
+  DetailScreen({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(item['title']!)),
+      body: Center(
+        child: Hero(
+          tag: item['id']!,
+          child: Image.asset(
+            item['image']!,
+            width: 300,
+            height: 400,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
